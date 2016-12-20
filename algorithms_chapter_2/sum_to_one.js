b995d92d6449677b8ca6febe6b92b834396e0650
William Hampton <willhamptonv@gmail.com>
2016-12-20 12:29:47 -0500

    added several algorithms and some assignments

--- algorithms_chapter_2/sum_to_one.js
+++ algorithms_chapter_2/sum_to_one.js
@@ -1,31 +1,13 @@
 // sum the input of a number to one digit ie 21 = 3 untill it is less than 10
 // so 2568 ==> 3
 
-function sumtoone(number){
-  var value = number
-  sum = value
-  while (sum >9){
-    sum = value
-        .toString()
-        .split('')
-        .map(Number)
-        .reduce(function (a, b) {
-            return a + b;
-        }, 0);
-        console.log(sum)
-        if (sum > 9){
-          newsum = sum
-          .toString()
-          .split('')
-          .map(Number)
-          .reduce(function (a, b) {
-              return a + b;
-          }, 0);
-          console.log(newsum)
-
-        }
-      }
-
-    console.log(sum);
+function sumDigits(number) {
+    var remainder = number % 10;
+    var sum = remainder;
+    if(number >= 10) {
+        var rest = Math.floor(number / 10);
+        sum += sumDigits(rest);
+    }
+    return sum;
 }
-sumtoone(2568);
+console.log(sumDigits(25))
